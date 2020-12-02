67a8ad7ad55d1e8e7450307ed6594ac55c51bfa4
Mayank <70268410+Mayank300@users.noreply.github.com>
2020-12-02 20:43:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//var message = document.getElementById("messages");
+
+
+// button.addEventListener("click", function(){
+//     var newMessage = document.createElement("li");
+//     newMessage.innerHTML = textbox.value;
+//     messages.appendChild(newMessage);
+//     textbox.value = "";
+// });
+
+
+
+
+
